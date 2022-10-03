d154a7538a7ea4a5529cc678d5a1becb4a6e2e45
DanyloSPTech <danylo.gomes@sptech.school>
2022-10-03 00:49:54 -0300

    view media uso componente

--- view-v1.sql
+++ view-v1.sql
@@ -4,6 +4,7 @@ CREATE VIEW DadosServidor
 AS
 	SELECT
 		idMaquina,
+        fkComponente,
         nomeComponente,
         registro,
         momento,
@@ -23,7 +24,7 @@ AS
 		idMaquina,
 		fkComponente,
         nomeComponente,
-		AVG(registro) AS 'UsoMedio'
+		ROUND(AVG(registro), 2) AS 'MediaUso'
 	FROM Maquina
 	INNER JOIN
 		Componente ON Maquina.idMaquina = Componente.fkMaquina
@@ -31,6 +32,17 @@ AS
 		Medida ON Componente.fkMedida = Medida.idMedida
 	INNER JOIN
 		Dados ON Componente.idComponente = Dados.fkComponente
+	GROUP BY fkComponente;
+    
+CREATE VIEW MediaUsoComponente
+AS
+	SELECT
+		idMaquina,
+        fkComponente,
+        nomeComponente,
+        ROUND(AVG(registro), 2) AS 'MediaUso'
+	FROM
+		DadosServidor
 	GROUP BY fkComponente;
         
 SELECT * FROM DadosServidor;
@@ -41,4 +53,4 @@ DROP VIEW DadosServidor;
 
 DROP VIEW MediaUsoComponente;
 
-select registro, DATE_FORMAT(momento,'%d/%m/%Y %H:%i:%s') as momento_grafico from DadosServidor where idMaquina = 1 order by idMaquina desc limit 7;
+select fkComponente, nomeComponente, registro, unidadeMedida, DATE_FORMAT(momento,'%d/%m/%Y %H:%i:%s') as momento_grafico from DadosServidor where idMaquina = 1 order by idMaquina desc limit 15;
